76face53d7a95daa5436ddfce79ce9394fd4099d
JonoKan2001 <79693698+JonoKan2001@users.noreply.github.com>
2022-11-16 16:22:31 +1300

    mvp for start of game page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css'
 import Home from './components/Home'
 import Create from './components/Create'
+import Start from './components/GameStart'
+
 const App = () => {
     return (
         <div>
             {/* <Home /> */}
-            <Create />
+            {/* <Create /> */}
+            <Start />
         </div>
     )
 }
