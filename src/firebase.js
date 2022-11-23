794f7f45a8b2d8d13d3c2ea2aa3528b78162dbc3
JonoKan2001 <79693698+JonoKan2001@users.noreply.github.com>
2022-11-23 19:34:42 +1300

    Add firebase config to app

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC316Bu6M8juKMvm5oM83uRi7Imm1TIv8g",
+  authDomain: "pokewho-92ec5.firebaseapp.com",
+  projectId: "pokewho-92ec5",
+  storageBucket: "pokewho-92ec5.appspot.com",
+  messagingSenderId: "802107260211",
+  appId: "1:802107260211:web:932731f954cc04073bd246",
+  measurementId: "G-FNJX9T5V7P"
+};
